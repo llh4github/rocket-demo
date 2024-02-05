5307db1612ef150d4d4b43a02cf08702b592a142
llh4github <61351434+llh4github@users.noreply.github.com>
2024-02-05 18:03:34 +0800

    ✨(docs): API文档

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
 #[macro_use]
 extern crate rocket;
+// use crate::config::ApiDoc;
 use log::info;
 use log::warn;
 use log4rs;
-#[get("/")]
-fn hello() -> &'static str {
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+mod config;
+
+#[utoipa::path(
+    context_path = "",
+    responses(
+        (status = 200, description = "get test request")
+    )
+)]
+#[get("/hello")]
+pub fn hello() -> &'static str {
     info!("info");
     warn!("warn");
     "Hello, world!"
 }
-
+#[derive(OpenApi)]
+#[openapi(
+    paths(::hello,),
+    tags((name = "roket-demo", description = "Demo endpoints.")),
+)]
+struct ApiDoc;
 #[launch]
 fn rocket() -> _ {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
     info!("booting up");
-    rocket::build().mount("/", routes![hello])
+    rocket::build()
+        .mount(
+            "/",
+            SwaggerUi::new("/swagger-ui/<_..>").url("/api-docs/openapi.json", ApiDoc::openapi()),
+        )
+        .mount("/", routes![hello])
 }
