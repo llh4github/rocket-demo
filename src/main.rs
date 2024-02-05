fe7f6a0e435103b03266cd503c79c6dccb881d85
llh4github <61351434+llh4github@users.noreply.github.com>
2024-02-05 17:07:25 +0800

    ✨(log): 集成日志

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+use log::info;
+use log::warn;
+use log4rs;
+#[get("/")]
+fn hello() -> &'static str {
+    info!("info");
+    warn!("warn");
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+    info!("booting up");
+    rocket::build().mount("/", routes![hello])
 }
