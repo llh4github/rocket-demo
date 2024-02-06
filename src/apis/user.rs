0416fd777e93dfda2d30a27f80149c122c3e4e18
llh <lilinhong_coding@foxmail.com>
2024-02-06 21:18:42 +0800

    ♻️(api): 接口出入参数

--- src/apis/user.rs
+++ src/apis/user.rs
@@ -1,16 +1,16 @@
+use log::info;
 use rocket::routes;
 use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
 use utoipa::OpenApi;
 use utoipa::ToSchema;
-#[allow(dead_code)]
 
 /// utoipa 没法自动生成正确的tag，手动指定下。
 const TAG: &str = "用户信息接口";
 #[derive(OpenApi)]
 #[openapi(
     tags((name = "用户信息接口", description = "操作用户数据的")),
-    paths(get_list),
+    paths(get_list,add),
     components(
         schemas(User)
     ),
@@ -18,7 +18,7 @@ const TAG: &str = "用户信息接口";
 pub struct Routes;
 impl Routes {
     pub fn url_list() -> Vec<rocket::Route> {
-        routes![get_list]
+        routes![get_list, add]
     }
 }
 
@@ -31,10 +31,20 @@ impl Routes {
 pub fn get_list() -> Json<Vec<User>> {
     Json(vec![User::new()])
 }
+/// 添加用户数据
+#[utoipa::path(path = "/user/add", 
+    tag = TAG,
+    responses((status=200, body=Option<User>)))
+]
+#[post("/user/add", data = "<data>")]
+pub fn add(data: Json<User>) -> Json<Option<User>> {
+    info!("接收到的数据： {:?}", data);
+    Json(Some(data.0))
+}
 /// 模拟用户数据
 ///
 /// 接入数据库后删除
-#[derive(Serialize, Deserialize, ToSchema, Clone)]
+#[derive(Serialize, Deserialize, ToSchema, Clone, Debug)]
 pub struct User {
     /// 名称
     name: String,
