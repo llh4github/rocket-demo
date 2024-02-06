a61ebe7fe98b5623e487beb5e17a7ce7d36541a2
llh4github <61351434+llh4github@users.noreply.github.com>
2024-02-06 18:59:10 +0800

    🩹(api): 返回值文档

--- src/apis/user.rs
+++ src/apis/user.rs
@@ -1,7 +1,8 @@
 use rocket::routes;
 use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
 use utoipa::OpenApi;
-
+use utoipa::ToSchema;
 #[allow(dead_code)]
 
 /// utoipa 没法自动生成正确的tag，手动指定下。
@@ -9,7 +10,10 @@ const TAG: &str = "用户信息接口";
 #[derive(OpenApi)]
 #[openapi(
     tags((name = "用户信息接口", description = "操作用户数据的")),
-    paths(get_list)
+    paths(get_list),
+    components(
+        schemas(User)
+    ),
 )]
 pub struct Routes;
 impl Routes {
@@ -19,8 +23,27 @@ impl Routes {
 }
 
 /// 获取用户列表
-#[utoipa::path(path = "/user/list", tag = TAG)]
+#[utoipa::path(path = "/user/list", tag = TAG,responses((status=200,body=User)))]
 #[get("/user/list")]
-pub fn get_list() -> Json<&'static str> {
-    Json("list")
+pub fn get_list() -> Json<Vec<User>> {
+    Json(vec![User::new()])
+}
+/// 模拟用户数据
+///
+/// 接入数据库后删除
+#[derive(Serialize, Deserialize, ToSchema, Clone)]
+pub struct User {
+    /// 名称
+    name: String,
+    /// 年纪
+    #[schema(example = 1)]
+    age: i32,
+}
+impl User {
+    fn new() -> User {
+        User {
+            name: "Tom".to_string(),
+            age: 33,
+        }
+    }
 }
