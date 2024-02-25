687452fe261c160f791a920f545f9d7b63a38775
llh4github <61351434+llh4github@users.noreply.github.com>
2024-02-25 14:41:11 +0800

    🔥(config): 无用代码

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -7,30 +7,6 @@ use utoipa_swagger_ui::SwaggerUi;
 use crate::{apis, config::types::App};
 pub mod types;
 
-/// DB实例
-// static DB: tokio::sync::OnceCell<DatabaseConnection> = tokio::sync::OnceCell::const_new();
-
-// pub async fn get_db() -> &'static DatabaseConnection {
-//     DB.get_or_init(init_db).await
-// }
-// /// 初始化数据库连接
-// async fn init_db() -> DatabaseConnection {
-//     let cnf: App = App::figment().extract().unwrap();
-
-//     let url = cnf.db.url;
-//     let mut options = ConnectOptions::new(url);
-//     options.min_connections(cnf.db.min_conn);
-//     options.sqlx_logging_level(cnf.db.log_level);
-//     let db = Database::connect(options).await;
-//     let db = match db {
-//         Ok(conn) => conn,
-//         Err(e) => {
-//             panic!("db connection failed! {}", e)
-//         }
-//     };
-//     info!("数据库连接成功！");
-//     db
-// }
 
 #[derive(Database, Debug)]
 #[database("sea_orm")]
